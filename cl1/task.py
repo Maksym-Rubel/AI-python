6ef8baa5f59b8c3757f5ad162d68aa3344e4a8f0
Maksym-Rubel <rubelmaksum2404@gmail.com>
2026-04-06 20:24:13 +0300

    first commit

--- cl1/task.py
+++ cl1/task.py
@@ -8,18 +8,18 @@ df['OrderDate'] = pd.to_datetime(df['OrderDate'])
 df['TotalAmount'] = df['Quantity'] * df['Price']
 
 print("----------------ЗАВДАННЯ 3-------------------")
-print(f"a. Сумарний дохід: {df['TotalAmount'].sum()}")
-print(f"b. Середнє значення TotalAmount: {df['TotalAmount'].mean()}")
-print("c. Кількість замовлень по клієнтах:")
+print(f"  Сумарний дохід : {df['TotalAmount'].sum()}")
+print(f"  Середнє  TotalAmount: {df['TotalAmount'].mean()}")
+print(" Кількість замовлень по  клієнтах:")
 print(df['Customer'].value_counts())
 
-print("\n------------------ЗАВДАННЯ 4--------------------")
+print("\n------------------ ЗАВДАННЯ 4--------------------")
 print(df[df['TotalAmount'] > 500])
 
 print("\n-------------------------ЗАВДАННЯ 5----------------")
 print(df.sort_values(by='OrderDate', ascending=False))
 
-print("\n-----------------------ЗАВДАННЯ 6------------------")
+print("\n----------------------- ЗАВДАННЯ 6------------------")
 print(df[(df['OrderDate'] >= '2023-06-05') & (df['OrderDate'] <= '2023-06-10')])
 
 print("\n--------------------ЗАВДАННЯ 7----------------")
